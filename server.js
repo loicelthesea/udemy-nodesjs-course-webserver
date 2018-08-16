7d7b66cc8ea2c6877c21475c354f1372a7bece7f
Loic El Thesea <loic.elthesea@outlook.com>
2018-08-16 19:44:05 +0200

    Setup start scripts and heroku port for heroku deployments

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
